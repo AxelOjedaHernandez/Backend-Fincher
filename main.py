0e76c6f3279be98cc41ee645949f3cdc159f782a
Axel Ojeda <axelojedahernandez64@gmail.com>
2025-04-02 13:20:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # main.py
 from fastapi import FastAPI
 from routers import masbank, pubchem
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API de Búsqueda en MassBank")
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes (cualquier frontend)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 app.include_router(masbank.router, prefix="/massbank", tags=["MassBank"])
 app.include_router(pubchem.router, prefix="/pubchem", tags=["PubChem"])
